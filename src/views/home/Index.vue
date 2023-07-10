d13edcf8013cfee94f7b58b1b97e0a4dd6fd9393
rogerlyu <roger.lyu@quad.tw>
2023-07-10 09:56:04 +0800

    整理文章 讓首頁有內容

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -14,30 +14,176 @@ export default {
 <template>
     <main class="flex-grow-1">
         <div class="container">
+            <div class="col-md-12">
+  
+      <article class="blog-post">
+        <h2 class="blog-post-title">
+            理解urlencode和urldecode的作用及用法
+            <small>
+                本網站提供線上 轉換 : <router-link :to="'/tools/url'">轉換網址</router-link>
+            </small>
+        </h2>
+        <br/>
+        <blockquote class="blockquote">
+          <p>文章：理解urlencode和urldecode的作用及用法
+在網路程式設計中，經常需要對URL中的特殊字元進行編碼和解碼，以確保資料的傳輸和儲存的準確性。這時就會用到兩個重要的函數：urlencode和urldecode。本文將詳細解釋這兩個函數的作用和用法，並舉例說明其實際應用。</p>
+        </blockquote>
 
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">功能</th>
-                        <th scope="col">URL</th>
+        <h2>1. urlencode的作用和用法：</h2>
+        <br/>
+<p>urlencode是一種編碼方法，用於將URL中的非字母數字字元轉換為特殊格式，以便在URL中進行傳輸和儲存。它將字串中的特殊字元轉換為%XX的形式，其中XX是該字元的ASCII碼值的十六進制表示。</p>
+<p>urlencode的語法如下：</p>
+<code>urlencode(string, encoding='utf-8', errors='replace')</code>
+<ul>
+  <li>string: 需要編碼的字串。</li>
+  <li>encoding: 字串的編碼格式，預設為utf-8。</li>
+  <li>errors: 錯誤處理方式，預設為'replace'。</li>
+</ul>
+<p>下面是一個示例，展示如何使用urlencode對URL進行編碼：</p>
+<pre><code>import urllib.parse
+url = 'https://www.example.com/?q=hello world'
+encoded_url = urllib.parse.urlencode({'url': url})
+print(encoded_url)
+</code></pre>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    <template v-for="(routeItem, index) in menuList">
-                        <tr>
-                            <th scope="row">{{ index }}</th>
-                            <td>{{ routeItem.name }}</td>
-                            <td>
-                                <router-link :to="routeItem.path">
-                                    {{ routeItem.path }}
-                                </router-link>
-                            </td>
-                        </tr>
-                    </template>
-                </tbody>
-            </table>
+<p>輸出結果為：<code>url=https%3A%2F%2Fwww.example.com%2F%3Fq%3Dhello+world</code></p>
+<p>在上述示例中，我們使用urllib.parse模組的urlencode函數對URL進行編碼。原始URL中的空格被替換為%20，而其他特殊字元也被相應地編碼。</p>
+<h2>2. urldecode的作用和用法：</h2>
+<br/>
+<p>urldecode與urlencode相反，它用於對編碼後的URL進行解碼，將特殊格式的字串還原為原始形式。</p>
+<p>urldecode的語法如下：</p>
+<code>urldecode(string, encoding='utf-8', errors='replace')</code>
+<ul>
+  <li>string: 需要解碼的字串。</li>
+  <li>encoding: 字串的編碼格式，預設為utf-8。</li>
+  <li>errors: 錯誤處理方式，預設為'replace'。</li>
+</ul>
+<p>下面是一個示例，展示如何使用urldecode對編碼後的URL進行解碼：</p>
+<pre><code>import urllib.parse
+encoded_url = 'url=https%3A%2F%2Fwww.example.com%2F%3Fq%3Dhello+world'
+decoded_url = urllib.parse.unquote(encoded_url)
+print(decoded_url)
+</code></pre>
+
+<p>輸出結果為：<code>url=https://www.example.com/?q=hello world</code></p>
+<p>在上述示例中，我們使用urllib.parse模組的unquote函數對編碼後的URL進行解碼，將%20還原為空格，將其他特殊字元還原為原始形式。</p>
+<h2>urlencode和urldecode的應用場景：</h2>
+<br/>
+<ul>
+  <li>在構建GET請求時，urlencode常用於將參數編碼為URL的一部分，確保參數的可靠傳遞。</li>
+  <li>在處理URL路徑或查詢字串時，urldecode用於還原特殊字元，以便進行正確的資料處理和解析。</li>
+  <li>在處理使用者輸入時，urlencode可以對使用者提供的字串進行編碼，以防止惡意輸入破壞URL結構。</li>
+</ul>
+<p>總結：</p>
+<p>urlencode和urldecode是網路程式設計中常用的函數，用於對URL中的特殊字元進行編碼和解碼。通過使用這兩個函數，我們可以確保URL的正確傳輸和儲存，同時提高資料的安全性和可靠性。</p>
+        <hr>
+      </article>
+
+
+      <article class="blog-post">
+        <h2 class="blog-post-title">
+            文章：理解JSON（JavaScript Object Notation）
+
+            <small>
+                本網站提供線上 轉換 : <router-link :to="'/tools/json'">轉換網址</router-link>
+            </small>
+        </h2>
+        <br/>
+        <p>JSON（JavaScript Object Notation）是一種輕量級的數據交換格式，常用於跨平台的數據傳輸和存儲。它基於JavaScript語言的一種子集，具有易於理解和編寫的特點。JSON以純文本的形式表示結構化的數據，並使用簡潔的語法來描述對象和數組。</p>
+<p>JSON由兩種主要的數據結構組成：</p>
+<ol>
+  <li><strong>對象（Object）：</strong>由花括號（{}）包圍，內部包含一組鍵值對（Key-Value pairs）。每個鍵值對中，鍵（Key）是一個字符串，值（Value）可以是字符串、數字、布爾值、對象、數組或null。</li>
+  <li><strong>數組（Array）：</strong>由方括號（[]）包圍，內部包含一組有序的值。數組中的值可以是字符串、數字、布爾值、對象、數組或null。</li>
+</ol>
+<p>下面是一個JSON的例子，展示了一個包含對象和數組的結構：</p>
+<pre><code>{
+  "name": "John Doe",
+  "age": 30,
+  "isStudent": false,
+  "hobbies": ["reading", "coding", "playing guitar"],
+  "address": {
+    "street": "123 Main St",
+    "city": "New York",
+    "country": "USA"
+  }
+}</code></pre>
+<p>在上述例子中，我們有一個包含個人資訊的JSON對象。它包括姓名、年齡、是否為學生等屬性，以及興趣愛好和地址等複雜的結構。</p>
+<h2>JSON的特點：</h2>
+<ul>
+  <li><strong>易於讀寫：</strong>JSON使用簡潔的語法，易於人們閱讀和理解。同時，也方便開發人員進行編寫和解析。</li>
+  <li><strong>跨平台：</strong>JSON是一種獨立於平台和語言的數據格式，可以在不同編程語言和操作系統之間進行數據交換。</li>
+  <li><strong>廣泛支持：</strong>JSON得到了廣泛的支持，在許多編程語言中都有相應的JSON解析和編寫庫。</li>
+  <li><strong>靈活性：</strong>JSON的結構靈活，可以嵌套對象和數組，適應不同的數據場景。</li>
+  <li><strong>輕量級：</strong>相比於其他數據交換格式，如XML，JSON的文件大小更小，佔用更少的帶寬和存儲空間。</li>
+</ul>
+<p>JSON在許多應用中都被廣泛使用，特別是在Web開發和API設計中。它常用於客戶端和服務器之間的數據傳輸，以及數據的存儲和序列化。無論是在網頁應用、行動應用還是後端服務，JSON都扮演著重要的角色，提供了一種方便、通用的數據交換格式。</p>
+        <hr>
+      </article>
+
+
+
+      <article class="blog-post">
+        <h2 class="blog-post-title">
+            文章：理解不可逆加密（One-Way Hashing）
+
+            <small>
+                本網站提供線上 轉換 : <router-link :to="'/tools/one-way-encryption'">轉換網址</router-link>
+            </small>
+        </h2>
+        <br/>
+        <p>在資訊安全領域中，不可逆加密（One-Way Hashing）是一種加密方法，通常用於將敏感資料進行保護。不可逆加密的特點是無法從加密後的數據中還原出原始數據，因此也被稱為單向加密。</p>
+<p>不可逆加密使用一個稱為雜湊函數（Hash Function）的數學算法，將原始數據轉換成固定長度的雜湊值（Hash Value），通常表示為一個字串。這個過程是單向的，不可逆的，也就是說無法從雜湊值推導出原始數據。</p>
+<p>下面是一些常見的不可逆加密算法：</p>
+<ul>
+  <li><strong>MD5：</strong>MD5（Message Digest Algorithm 5）是一個常見的不可逆加密算法，產生的雜湊值為128位。然而，由於MD5存在一些安全漏洞，如碰撞攻擊，因此在安全性要求較高的場景中不建議使用。</li>
+  <li><strong>SHA系列：</strong>SHA（Secure Hash Algorithm）是一系列的不可逆加密算法，包括SHA-1、SHA-256、SHA-512等。這些算法產生的雜湊值長度不同，並且提供了更高的安全性。</li>
+  <li><strong>Bcrypt：</strong>Bcrypt是一種基於鹽（Salt）的不可逆加密算法，用於密碼的存儲和驗證。它通常用於保護密碼，並提供了額外的安全性功能，如適應性哈希、工作因子等。</li>
+</ul>
+<p>不可逆加密在資訊安全中有許多應用：</p>
+<ul>
+  <li><strong>密碼存儲：</strong>不可逆加密通常用於存儲密碼。當用戶註冊或修改密碼時，將其原始密碼應用不可逆加密算法，然後將得到的雜湊值存儲在資料庫中。在驗證密碼時，將用戶輸入的密碼與存儲的雜湊值進行比對，而不需要存儲明文密碼。</li>
+  <li><strong>數據完整性驗證：</strong>不可逆加密可用於驗證數據的完整性。將原始數據應用不可逆加密算法得到雜湊值，並將該雜湊值與接收方收到的數據進行比對。如果兩個雜湊值相同，則可以確定數據在傳輸過程中沒有被修改。</li>
+  <li><strong>數字簽名：</strong>不可逆加密可用於生成和驗證數字簽名。對於一個文件，將其應用不可逆加密算法得到雜湊值，然後使用私鑰進行加密，生成數字簽名。接收方可以使用相應的公鑰進行解密和驗證，確定文件的完整性和真實性。</li>
+</ul>
+<p>需要注意的是，儘管不可逆加密提供了一定程度的安全性，但仍然存在一些攻擊方法，如彩虹表攻擊和暴力破解。因此，在選擇不可逆加密算法時，應該考慮安全性和性能要求，並適當地應用其他安全措施，如鹽值和適應性哈希。</p>
+        <hr>
+      </article>
+
+
+      <article class="blog-post">
+        <h2 class="blog-post-title">
+            文章：理解可逆加密（Symmetric Encryption）
+
+            <small>
+                本網站提供線上 轉換 : <router-link :to="'/tools/reversible-encryption'">轉換網址</router-link>
+            </small>
+        </h2>
+        <br/>
+        <p>可逆加密（Symmetric Encryption）是一種加密方法，通常用於將敏感資料進行保護。可逆加密使用相同的密鑰（Key）進行加密和解密，可以將加密後的數據還原為原始數據。</p>
+<p>可逆加密的過程如下：</p>
+<ol>
+  <li>原始數據通過一個稱為加密算法的數學運算，使用密鑰作為參數進行加密。</li>
+  <li>加密後的數據稱為密文（Ciphertext），它是原始數據的不可讀形式。</li>
+  <li>接收方使用相同的密鑰進行解密操作，將密文還原為原始數據。</li>
+</ol>
+<p>常見的可逆加密算法有：</p>
+<ul>
+  <li><strong>DES（Data Encryption Standard）：</strong>DES是一個古老的可逆加密算法，使用56位密鑰。然而，DES的密鑰長度較短，容易受到暴力破解的攻擊。</li>
+  <li><strong>3DES：</strong>3DES（Triple Data Encryption Standard）是DES的一個改進版本，使用多次DES算法進行加密，提高了安全性。</li>
+  <li><strong>AES（Advanced Encryption Standard）：</strong>AES是一個較新且廣泛使用的可逆加密算法，支持不同的密鑰長度，包括128位、192位和256位。AES在安全性和效能上都具有較高的表現。</li>
+</ul>
+<p>可逆加密在資訊安全中有多種應用：</p>
+<ul>
+  <li><strong>數據傳輸保護：</strong>可逆加密常用於保護數據在傳輸過程中的機密性。發送方使用密鑰將數據加密成密文，接收方使用相同的密鑰解密密文，以獲取原始數據。</li>
+  <li><strong>數據存儲保護：</strong>可逆加密可用於保護數據在儲存過程中的機密性。數據在存儲之前使用密鑰進行加密，並在需要時使用相同的密鑰解密，以還原原始數據。</li>
+  <li><strong>身份驗證：</strong>可逆加密可用於保護身份驗證信息的機密性。例如，在使用者登錄過程中，將使用者密碼加密並存儲在資料庫中。在驗證過程中，將輸入的密碼進行加密，然後與存儲的加密密碼進行比對。</li>
+</ul>
+<p>需要注意的是，可逆加密要求傳送方和接收方都知道並使用相同的密鑰。因此，在使用可逆加密時，必須確保密鑰的安全性，避免未經授權的人員獲取密鑰，以保護數據的機密性。</p>
+        <hr>
+      </article>
+
+
+    </div>
         </div>
     </main>
 </template>
